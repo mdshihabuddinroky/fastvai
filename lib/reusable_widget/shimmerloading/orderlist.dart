202a7c35d9e6e199c07b9dfddf80cc813caf8bbe
Bdkoder360 <bdkoder360@gmail.com>
2022-02-23 11:53:40 +0600

    fastvai

---
+++ lib/reusable_widget/shimmerloading/orderlist.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:shimmer/shimmer.dart';
+
+class Ordershimmer extends StatelessWidget {
+  const Ordershimmer({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 200.0,
+      height: 100.0,
+      child: Shimmer.fromColors(
+        baseColor: Colors.red,
+        highlightColor: Colors.yellow,
+        child: const Text(
+          'Shimmer',
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            fontSize: 40.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/screens/Store/orderlist.dart
+++ lib/screens/Store/orderlist.dart
@@ -1,4 +1,6 @@
+import 'package:fastvai/screens/emptyscreen.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class Orderlist extends StatelessWidget {
   const Orderlist({Key? key}) : super(key: key);
@@ -6,7 +8,20 @@ class Orderlist extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('Order list'),
-    );
+        body: Column(
+      children: [
+        Ordershimmer();
+        // Spacer(),
+        // Center(
+        //   child: Empty(
+        //     icon: Icons.list_alt_outlined,
+        //     title: 'No Order History',
+        //     subtitle:
+        //         'You have no order history. Please make order & get fastest delivery in Bangladesh',
+        //   ),
+        // ),
+        // Spacer(),
+      ],
+    ));
   }
 }
